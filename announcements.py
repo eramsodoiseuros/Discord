a15d7774948bf2e3662597be6eb6ad35c3039caf
Francisco Peixoto <thtr.irubra@gmail.com>
2022-10-20 11:41:14 +0100

    fetch members, fetch roles

--- announcements.py
+++ announcements.py
@@ -15,4 +15,28 @@ async def on_message(message):
 
   if message.channel.name == 'announcements':
     
+@client.command()
+async def hi(ctx):
+    with open('users.txt','w') as f:
+        async for member in ctx.guild.fetch_members(limit=None):
+            print("{},{}".format(member,member.id), file=f,)
+    print("done")
+    
+@client.command()
+async def info(ctx, user: discord.Member):
+
+    mention = []
+    for role in user.roles:
+        if role.name != "@everyone":
+            mention.append(role.mention)
+
+    b = ", ".join(mention)
+
+
+    embed = discord.Embed(title="Info:", description=f"Info of: {user.mention}", color=discord.Color.orange())
+    embed.add_field(name="Top role:", value=user.top_role)
+    embed.add_field(name="Roles:", value=b)
+
+    await ctx.send(embed=embed)
+    
 client.run(TOKEN)
